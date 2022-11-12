01760a459b7556281ec9489d28285a3de9141400
Zominik <44556744+Ideefixze@users.noreply.github.com>
2022-11-12 23:51:43 +0100

    Update vast-ai-config.sh

--- vast-ai-config.sh
+++ vast-ai-config.sh
@@ -5,15 +5,15 @@ apt upgrade -y
 apt install -y libgl1 transmission-cli screen python3-venv
 
 git clone https://github.com/Ideefixze/stable-diffusion-webui-vast-ai.git
-cd stable-diffusion-webui
+cd stable-diffusion-webui-vast-ai
 
 conda create -n "stable-diff" python=3.10.4 -y
 conda init bash
 bash
 conda activate stable-diff
 
-cd /workspace/stable-diffusion-webui/models
+cd /workspace/stable-diffusion-webui-vast-ai/models
 wget https://huggingface.co/hakurei/waifu-diffusion-v1-3/resolve/main/wd-v1-3-full.ckpt
 
-cd /workspace/stable-diffusion-webui
+cd /workspace/stable-diffusion-webui-vast-ai
 ./webui.sh
